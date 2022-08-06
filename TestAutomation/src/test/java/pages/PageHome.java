1ce673a6b9acc858267382bad2014c32292a14c7
Ivan Maldonado <maldonado.ivan95@gmail.com>
2022-08-05 23:30:51 -0300

    Refactor name project

---
+++ TestAutomation/src/test/java/pages/PageHome.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class PageHome {
+	private WebDriver driver;
+	private By jobsDrop;
+	
+	public PageHome(WebDriver driver) {
+		this.driver = driver;
+		jobsDrop = By.id("menu_admin_Job");
+	}
+	
+	public void selectJobs(int cant) {
+		Select selectJobList = new Select(driver.findElement(jobsDrop));
+		selectJobList.selectByVisibleText(null);
+	}
+}
